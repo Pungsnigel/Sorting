280b184054f8f604b8e39cdf008ebd55f3e57392
Jesper Persson <JesperT.Persson@gmail.com>
2013-11-14 13:16:37 +0100

    Intend code more beautiful in quicksort.java

--- src/quicksort.java
+++ src/quicksort.java
@@ -9,7 +9,7 @@ public class quicksort {
 		while (i <= j) {
 			for(; array[i] < pivot;	i++) ; //Increment i until value lower than pivot is found
 			for(; array[j] > pivot; j--) ; //Decrement j until value higher than pivot is found
-			if (i <= j) {				   // Make sure the current iteration has not put us passed i <= j.
+			if (i <= j) {		      // Make sure the current iteration has not put us passed i <= j.
 				swap(array, i, j);
 				i++;
 				j--;
@@ -25,8 +25,8 @@ public class quicksort {
 	}
  
 	private static void swap(int[] array, int left, int right) {
-		int tmp = array[left];
-		array[left] = array[right];
+		int tmp      = array[left];
+		array[left]  = array[right];
 		array[right] = tmp;
 	}
 }
